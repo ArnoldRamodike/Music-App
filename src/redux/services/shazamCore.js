1a8059965ebacc9d61e26b3643b34fd2c75f15ad
ArnoldRamodike <riccoarnold23@gmail.com>
2022-11-24 17:36:01 +0200

    Around you Page

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -17,7 +17,9 @@ import { createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
         getSongRelated: builder.query({ query: ({songid}) => `/tracks/related?track_id=${songid}`}),
 
-        getArtistDetails: builder.query({ query: (artistId) => `artists/details?artist_id=${artistId}`})
+        getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}`}),
+
+        getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}`}),
          }),
     });
 
@@ -26,4 +28,5 @@ import { createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
         useGetSongDetailsQuery,
         useGetSongRelatedQuery,
         useGetArtistDetailsQuery,
+        useGetSongsByCountryQuery
     } = shazamCoreAPI;
